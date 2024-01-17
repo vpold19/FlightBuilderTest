dd15471cd4119bc372b8211cfd03de93419899e1
vadim <vadimpoldolin19@gmail.com>
2024-01-18 00:09:44 +0100

    refactoring 1.1

--- src/main/java/com/gridnine/testing/TestFlights/Main.java
+++ src/main/java/com/gridnine/testing/TestFlights/Main.java
@@ -6,10 +6,10 @@ import java.util.List;
 
 public class Main {
     public static void main(String[] args) {
-        List<Flight> allFlights = FlightBuilder.createFlights();
+        List<Flight> flights = FlightBuilder.createFlights();
         FlightServiceImpl flightService = new FlightServiceImpl();
-        flightService.departureToTheCurrentTime(allFlights);
-        flightService.segmentsWithArrivalDateEarlierThanDepartureDate(allFlights);
-        flightService.flightsWhereTotalTimeSpentOnGroundTwoHours(allFlights);
+        flightService.departureToTheCurrentTime(flights);
+        flightService.segmentsWithArrivalDateEarlierThanDepartureDate(flights);
+        flightService.flightsWhereTotalTimeSpentOnGroundTwoHours(flights);
     }
 }
